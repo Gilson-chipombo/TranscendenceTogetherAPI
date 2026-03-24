fc14b72d1309de3d7e12b0b2de2af988a18d494b
Gilson Bravo  Chipombo <116081541+Gilson-chipombo@users.noreply.github.com>
2026-03-24 11:28:56 +0100

    Refactor RedisService constructor to use REDIS_URL

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -5,16 +5,20 @@ import Redis from 'ioredis'
 @Injectable()
 export class RedisService extends Redis implements OnModuleInit, OnModuleDestroy {
     private readonly logger = new Logger(RedisService.name);
-    constructor(){
-        super({
-                host: process.env.HOST_REDIS_REMOTE,
-                password: process.env.REDISPASS,
-                port: Number(process.env.PORT_REDIS),
-                username: process.env.USER_REDIS,
-                tls: process.env.NODE_ENV === 'production' ? {} : null,
-                maxRetriesPerRequest: null,
-            })
-        }
+    // constructor(){
+    //     super({
+    //             host: process.env.HOST_REDIS_REMOTE,
+    //             password: process.env.REDISPASS,
+    //             port: Number(process.env.PORT_REDIS),
+    //             username: process.env.USER_REDIS,
+    //             tls: process.env.NODE_ENV === 'production' ? {} : null,
+    //             maxRetriesPerRequest: null,
+    //         })
+    //     }
+      constructor() {
+        super(process.env.REDIS_URL);
+      }
+
     async onModuleInit(){
         this.on('connect', () =>{
             this.logger.log("Resdis conected sucessiful");
